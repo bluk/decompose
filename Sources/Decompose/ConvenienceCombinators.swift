f2909e3a7d5aff03c0f01331bbb74d989ffd3bc4
Bryant Luk <bryant.luk@bryantluk.com>
2018-09-06 14:33:52 -0500

    Fix code formatting

--- Sources/Decompose/ConvenienceCombinators.swift
+++ Sources/Decompose/ConvenienceCombinators.swift
@@ -20,30 +20,31 @@ public extension Combinators {
     /// Return a Parser which tests if the next value is a specific Character
     static func char<Input1, Input2>(_ value: Character) -> Parser<Input1, Input2, Character>
         where Input1.Element == Character {
-            return satisfy { $0 == value }
+        return satisfy { $0 == value }
     }
 
     /// Return a Parser which tests if the next value is a letter
     static func isLetter<Input1, Input2>() -> Parser<Input1, Input2, Character>
         where Input1.Element == Character {
-            let characterSet = CharacterSet.letters
-            #if swift(>=4.2)
-            return satisfy { $0.unicodeScalars.allSatisfy(characterSet.contains) }
-            #else
-            // https://github.com/apple/swift-evolution/blob/master/proposals/0207-containsOnly.md
-            return satisfy { !$0.unicodeScalars.contains { !characterSet.contains($0) } }
-            #endif
+        let characterSet = CharacterSet.letters
+        #if swift(>=4.2)
+        return satisfy { $0.unicodeScalars.allSatisfy(characterSet.contains) }
+        #else
+        // https://github.com/apple/swift-evolution/blob/master/proposals/0207-containsOnly.md
+        return satisfy { !$0.unicodeScalars.contains { !characterSet.contains($0) } }
+        #endif
     }
 
     /// Return a Parser which tests if the next value is a digit
     static func isDigit<Input1, Input2>() -> Parser<Input1, Input2, Character>
         where Input1.Element == Character {
-            let characterSet = CharacterSet.decimalDigits
-            #if swift(>=4.2)
-            return satisfy { $0.unicodeScalars.allSatisfy(characterSet.contains) }
-            #else
-            // https://github.com/apple/swift-evolution/blob/master/proposals/0207-containsOnly.md
-            return satisfy { !$0.unicodeScalars.contains { !characterSet.contains($0) } }
-            #endif
+        let characterSet = CharacterSet.decimalDigits
+        #if swift(>=4.2)
+        return satisfy { $0.unicodeScalars.allSatisfy(characterSet.contains) }
+        #else
+        // https://github.com/apple/swift-evolution/blob/master/proposals/0207-containsOnly.md
+        return satisfy { !$0.unicodeScalars.contains { !characterSet.contains($0) } }
+        #endif
     }
+
 }
