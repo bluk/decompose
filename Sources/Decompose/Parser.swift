d73756fe7de5dda19902804dad80c4bd00900c09
Bryant Luk <bryant.luk@bryantluk.com>
2018-09-07 11:05:33 -0500

    Document Parser

--- Sources/Decompose/Parser.swift
+++ Sources/Decompose/Parser.swift
@@ -12,14 +12,22 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-/// A parsable function
+/// A value type for the `parse` function.
 public struct Parser<I, V> where I: Input {
-    let parse: (I) -> Consumed<I, V>
+
+    /// A function which takes an `Input` and returns a type which indicates if the `Input` is consumed when parsing
+    /// the result and either a parsed value or an error message.
+    public let parse: (I) -> Consumed<I, V>
 }
 
-/// Convenience methods for Parser
+/// Convenience methods for `Parser`.
 public extension Parser {
-    /// Convenience method for binding a first parser's return value to a second parser.
+
+    /// Binds this `Parser`'s return value to a function which generates another `Parser`.
+    ///
+    /// - Parameters:
+    ///     - func1: The function to bind the value of this parser to.
+    /// - Returns: A bound `Parser`.
     func flatMap<V2>(
         _ func1 : @escaping (V) -> Parser<I, V2>) -> Parser<I, V2> {
         return Combinators.bind(self, to: func1)
